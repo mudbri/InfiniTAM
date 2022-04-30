d2c8c0177f6d786f7be31a9ff19f46525e9d6a24
mudbri <mubashiranwar@live.com>
2022-04-29 21:04:18 -0500

    Added Hashentries to CUDA
    Not tested since cmake is without CUDA

--- InfiniTAM/ITMLib/Engines/Meshing/CUDA/ITMMeshingEngine_CUDA.h
+++ InfiniTAM/ITMLib/Engines/Meshing/CUDA/ITMMeshingEngine_CUDA.h
@@ -19,7 +19,7 @@ namespace ITMLib
 		Vector4s *visibleBlockGlobalPos_device;
 
 	public:
-		void MeshScene(ITMMesh *mesh, const ITMScene<TVoxel, ITMVoxelBlockHash> *scene);
+		void MeshScene(ITMMesh *mesh, const ITMScene<TVoxel, ITMVoxelBlockHash> *scene, std::vector<ITMHashEntry>& hashEntries);
 
 		ITMMeshingEngine_CUDA(void);
 		~ITMMeshingEngine_CUDA(void);
@@ -29,7 +29,7 @@ namespace ITMLib
 	class ITMMeshingEngine_CUDA<TVoxel, ITMPlainVoxelArray> : public ITMMeshingEngine < TVoxel, ITMPlainVoxelArray >
 	{
 	public:
-		void MeshScene(ITMMesh *mesh, const ITMScene<TVoxel, ITMPlainVoxelArray> *scene);
+		void MeshScene(ITMMesh *mesh, const ITMScene<TVoxel, ITMPlainVoxelArray> *scene, std::vector<ITMHashEntry>& hashEntries);
 
 		ITMMeshingEngine_CUDA(void);
 		~ITMMeshingEngine_CUDA(void);
